dc1bdc1cc705febc02b32ab31a263be98117887c
John <John@DESKTOP-IBS61H3>
2023-01-20 14:42:36 +0900

    날아가용

---
+++ sweet/src/main/java/com/kafka/SweetApplication.java
@@ -0,0 +1,13 @@
+package com.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SweetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SweetApplication.class, args);
+	}
+
+}
